0bf1701c56e3b9cec3208e6c807d03b5e4915e0f
Masoudi Parrot <masoudi@localhost.localdomain>
2022-04-29 10:16:01 -0400

    add index.css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
